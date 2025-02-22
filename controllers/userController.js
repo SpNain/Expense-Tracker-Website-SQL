6b1409303869b41415ea6f92d3195b4f6f085cd5
HardeepNain <hnspnain@gmail.com>
2025-02-22 23:09:05 +0530

    Created login backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,3 +33,32 @@ exports.postUserSignUp = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postUserLogin = (req, res, next) => {
+  const email = req.body.loginEmail;
+  const password = req.body.loginPassword;
+
+  User.findOne({ where: { email: email } }).then((user) => {
+    if (user) {
+      if (user.password == password) {
+        res
+          .status(200)
+          .send(
+            `<script>alert('Login Successful!'); window.location.href='/'</script>`
+          );
+      } else {
+        res
+          .status(401)
+          .send(
+            `<script>alert('Password Incorrect!'); window.location.href='/'</script>`
+          );
+      }
+    } else {
+      res
+        .status(404)
+        .send(
+          `<script>alert("User doesn't Exists!"); window.location.href='/'</script>`
+        );
+    }
+  });
+};
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/", userController.getLoginPage);
-
+router.post("/login", userController.postUserLogin);
 router.post("/signUp", userController.postUserSignUp);
 
 module.exports = router;

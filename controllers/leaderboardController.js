f68a4a2f7807ed70df9c7eeffdf045175db5c4d5
HardeepNain <hnspnain@gmail.com>
2025-02-22 23:26:48 +0530

    Optimized code to get data for leaderboard from database

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const User = require("../models/userModel");
-const Expense = require("../models/expenseModel");
 const sequelize = require("../util/database");
 
 exports.getLeaderboardPage = (req, res, next) => {
@@ -9,6 +8,8 @@ exports.getLeaderboardPage = (req, res, next) => {
   );
 };
 
+/*
+OPTIMIZED
 exports.getLeaderboard = (req, res, next) => {
   Expense.findAll({
     attributes: [
@@ -32,4 +33,27 @@ exports.getLeaderboard = (req, res, next) => {
       res.send(JSON.stringify(result));
     })
     .catch((err) => console.log(err));
+};
+*/
+
+// MORE OPTIMIZED
+exports.getAllUsersForLeaderboard = (req, res, next) => {
+  User.findAll({
+    attributes: [
+      [sequelize.col("name"), "name"],
+      [sequelize.col("totalExpenses"), "totalExpenses"],
+    ],
+    order: [[sequelize.col("totalExpenses"), "DESC"]],
+  })
+    .then((users) => {
+      const result = users.map((user) => ({
+        name: user.getDataValue("name"),
+        totalExpenses: user.getDataValue("totalExpenses"),
+      }));
+      res.send(JSON.stringify(result));
+    })
+    .catch((err) => {
+      console.error('Error fetching users for leaderboard:', err);
+      res.status(500).send('An error occurred while retrieving the leaderboard.');
+    });
 };
